64318644a98439e4881ebb34e8044895f826712b
Karl Osterberg <karost0249@gmail.com>
2025-07-05 10:28:34 +0200

    Fix crash when launching

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // Application won't start on Wayland with Nvidia
+    // Full discussion: https://github.com/tauri-apps/tauri/issues/10702
+    // std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    std::env::set_var("__GL_THREADED_OPTIMIZATIONS", "0");
+    std::env::set_var("__NV_DISABLE_EXPLICIT_SYNC", "1");
     swww_itch_lib::run()
 }
